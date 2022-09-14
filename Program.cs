309bb734b55e869a3d7b572e902e76253d03534a
Folasayo Samuel <folasayosamuel@gmail.com>
2022-09-14 16:20:56 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+	class Program
+	{
+		static void Main(string[]args)
+		{
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.Write("Enter a number: ");
+			var num1 = Convert.ToInt32(Console.ReadLine());
+			
+			Console.Write("Enter another number: ");
+			var num2 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine(num1 + num2);
+			
+			Console.ReadLine();
+
+		}
+	}
+}
